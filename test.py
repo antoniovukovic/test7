9788e4f75af272743c731bf0ae99437601a0b695
antoniovukovic <antonio@amadesign.hr>
2017-06-01 16:06:26 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+secretNum = 6
+
+test = int(raw_input("Odaberi jedan broj, sigurno ćeš pogriješiti: "))
+
+if test == secretNum:
+    print("Bravo, 6.")
+else:
+    print("Nisi pogodio prijatelju")
